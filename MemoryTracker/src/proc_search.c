60347e8265cdd7ff719260149034f0800477a213
Jin Minu <s9430939@naver.com>
2025-11-02 21:29:34 +0900

    test2

--- MemoryTracker/src/proc_search.c
+++ MemoryTracker/src/proc_search.c
@@ -23,7 +23,7 @@ FILE *open_proc_stat(pid_t pid) {
 
 	status_fd = fopen(full_proc_path, "r");
 	if (status_fd == NULL) {
-		cusor_to(12, 1);
+		cursor_to(12, 1);
 		printf("관찰중인 프로세스가 죽었음\n");
 		return NULL; // exit(1)은 프로그램이 그냥 죽어버림, 따라서 NULL리턴
 	}
